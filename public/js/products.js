bb96b524b2787a2b3199918c917a5ac16206ca71
Egor Danilkin <egr13.2011@gmail.com>
2021-05-10 23:25:52 +0700

    'dz'

---
+++ public/js/products.js
@@ -0,0 +1,14 @@
+Vue.component('products-comp',{
+    props:['showed','addproduct'],
+    template: ` <div class="products">
+                    <div v-for="product of showed" :key='product.id_product' class="product-item">
+                        <img :src="product.img" alt="some img" class='product_img'>
+                        <div class='product_info'>
+                            <h3 class='product_name'>{{product.product_name}}</h3>
+                            <p class='product_price'>Стоимость: {{product.price}} рублей</p>
+                            <button class="buy-btn" :id="product.id_product" @click='$emit("addproduct",product)'>Купить</button>
+                        </div>
+                    </div>
+                </div>
+`
+})
